ef5536c30463ab7a9dcb9814891dc13389036036
4283246 <102957536+4283246@users.noreply.github.com>
2022-04-21 16:08:32 +0200

    added completed code

--- HangmanGameSln/HangmanGameSln/Hangman.Core/Game/Hangman.cs
+++ HangmanGameSln/HangmanGameSln/Hangman.Core/Game/Hangman.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using HangmanRenderer.Renderer;
 
 namespace Hangman.Core.Game
@@ -7,26 +8,213 @@ namespace Hangman.Core.Game
     {
         private GallowsRenderer _renderer;
 
+        List<string> _wordDictionary = new List<string> { "bottle", "code", "speed", "snapchat", "radio", "random", "rands", "hands", "robot", "drive", "truck", "dance", "string", "license", "game", "phone", "time", "spoon", "cake", "drunk" };
+
+        private string _randomWord;
+        private string _guess;
+        private int _numberOflives = 6;
+
         public HangmanGame()
         {
             _renderer = new GallowsRenderer();
-        }
 
-        public void Run()
-        {
-            _renderer.Render(5, 5, 6);
+            Random random = new Random();
+            
+           int index = random.Next(_wordDictionary.Count);
+           _randomWord = _wordDictionary[index];
 
-            Console.SetCursorPosition(0, 13);
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Write("Your current guess: ");
-            Console.WriteLine("--------------");
-            Console.SetCursorPosition(0, 15);
+            for(int i=0; i < _randomWord.Length; i++)
+            {
+                _guess = _guess + "_";
+            }
+
+        //   _guess = guess;
+
+      }
+
+      public void Run()
+      {
+
+       //   _renderer.Render(10, 5, 0);
+
+          Console.SetCursorPosition(0, 13);
+          Console.ForegroundColor = ConsoleColor.Blue;
+          
+            
+
+            while(_numberOflives > 0 && _numberOflives <=6)
+            {
+                _renderer.Render(10, 5, _numberOflives);
+
+                Console.SetCursorPosition(0, 15);
+
+                Console.Write("Your current guess: ");
+                Console.WriteLine(_guess);
+
+
+                Console.Write("What is your next guess: ");
+                var nextGuess = Console.ReadLine();
+
+                char[] guessArray = _guess.ToCharArray();
+
+                bool correct = false;
+
+
+                for(int i=0; i < _randomWord.Length; i++)
+                { 
+                    if (_randomWord[i] == nextGuess[0])
+                    {
+                        guessArray[i] = _randomWord[i];
+                        correct = true;
+                        //we know its correct
+                    }
+
+                    //it was not correct
+                }
+
+                _guess = new string(guessArray);
+
+
+                if (!correct)
+                    _numberOflives--;
+            
+            }
+
+
+
+            /*
+            foreach (string word in _wordDictionary)
+            {
+                if (_guess == word)
+                {
+                    _wordDictionary = false; 
+                    _guess = word;
+                    Console .Write(word);
+                }
+                else
+                {
+                    if (word == _guess) 
+                    {
+                        Console.Write("Please enter next guess:");
+                    }
+                    if (word3 == _guess)
+                    
+                }
+                       
+            }
 
             Console.ForegroundColor = ConsoleColor.Green;
 
-            Console.Write("What is your next guess: ");
-            var nextGuess = Console.ReadLine();
+          Console.Write("What is your next guess: ");
+          var nextGuess = Console.ReadLine();
+            */
+            // Game.
+
+
         }
 
+        /*        private static int HangmanGame(int wrong)
+              {
+
+                  if (wrong == 0)
+                  {
+
+                      Console.WriteLine("\n+---+");7
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    ===");
+
+                  }
+                  else if (wrong == 1)
+                  {
+                      Console.WriteLine("\n+---+");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    ===");
+
+                  }
+
+                  else if (wrong == 2)
+                  {
+                      Console.WriteLine("\n+---+");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    ===");
+
+                  }
+                  else if (wrong == 3)
+                  {
+                      Console.WriteLine("\n+---+");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    ===");
+                  }
+                  else if (wrong == 4)
+                  {
+                      Console.WriteLine("\n+---+");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    ===");
+                  }
+                  else if (wrong == 5)
+                  {
+                      Console.WriteLine("\n+---+");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    ===");
+                  }
+                  else if (wrong == 6)
+                  {
+                      Console.WriteLine("\n+---+");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    |");
+                      Console.WriteLine("    ===");
+                  }
+
+
+
+              }
+
+              private static int printWord(List<char> guessedLetters, string randomworld)
+              {
+                  int count = 0;
+                  int rightLetters = 0;
+                  Console.WriteLine("\r\n");
+                  foreach (char c in randomworld)
+                  {
+                      if (guessedLetters.Contains(c))
+                      {
+                          Console.Write(c + " ");
+                          rightLetters++;
+                      }
+                      else
+                      {
+                          Console.Write(" ");
+
+                      }
+                      count++;
+                  }
+                  return rightLetters;
+              }
+
+              private static void printLines(string randomWord)
+              {
+                  Console.Write("\r");
+                  foreach (char c in randomWord)
+                  {
+                      Console.OutputEncoding = System.Text.Encoding.Unicode;
+                      Console.Write("\u0305");
+                  }
+
+              }
+        */
+
     }
-}
+    }
--- HangmanGameSln/HangmanGameSln/HangmanGame/Program.cs
+++ HangmanGameSln/HangmanGameSln/HangmanGame/Program.cs
@@ -2,9 +2,10 @@
 using Hangman.Core.Game;
 
 namespace HangmanGameConsole
-{
+{ 
+
     internal class Program
-    {
+    { 
         static void Main(string[] args)
         {
             ConsoleColor oldColor = Console.ForegroundColor;
@@ -12,11 +13,14 @@ namespace HangmanGameConsole
             Console.ForegroundColor = ConsoleColor.Green;
             Console.SetCursorPosition(10, 2);
             Console.Write("Welcome to Hangman!!");
-
+             Console.WriteLine("------------------------------------");
+        
+         //   string guess = Console.ReadLine();
             var hangman = new HangmanGame();
             hangman.Run();
 
             Console.ForegroundColor = oldColor;
+
 
 
             Console.SetCursorPosition(20, 25);
